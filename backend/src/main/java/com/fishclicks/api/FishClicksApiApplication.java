8f4448114f2e72bdfecb3abcbac17649bb906d2d
Niett204 <toninm2004@gmail.com>
2026-03-05 21:06:04 +0100

    add spring boot backend

---
+++ backend/src/main/java/com/fishclicks/api/FishClicksApiApplication.java
@@ -0,0 +1,13 @@
+package com.fishclicks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishClicksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishClicksApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fishclicks/api/FishClicksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishclicks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishClicksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
