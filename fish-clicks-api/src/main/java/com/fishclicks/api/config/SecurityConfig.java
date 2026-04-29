8d2e2b89401ad33f8e636f08a7cd80b133d78ab2
Juanfra <juanfra.riera2003@gmail.com>
2026-04-29 18:20:40 +0200

    La contraseña se encripta

--- fish-clicks-api/src/main/java/com/fishclicks/api/config/SecurityConfig.java
+++ fish-clicks-api/src/main/java/com/fishclicks/api/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 
@@ -50,6 +52,11 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
