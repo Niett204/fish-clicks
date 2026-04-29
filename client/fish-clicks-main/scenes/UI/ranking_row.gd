3d021afa48d46eb0775d4f5b27228288df94d300
Juanfra <juanfra.riera2003@gmail.com>
2026-04-29 17:54:14 +0200

    Ranking con colores nuevos

--- client/fish-clicks-main/scenes/UI/ranking_row.gd
+++ client/fish-clicks-main/scenes/UI/ranking_row.gd
@@ -12,11 +12,11 @@ func set_data(pos: int, nickname: String, score: String, _foto_base64: String):
 	
 	if nickname == GlobalData.user_nickname:
 		# Esto cambia el color de la fuente directamente, ignorando el marrón del inspector
-		name_label.add_theme_color_override("font_color", Color(1, 1, 0))
-		rank_label.add_theme_color_override("font_color", Color(1, 1, 0))
-		value_label.add_theme_color_override("font_color", Color(1, 1, 0))
+		name_label.add_theme_color_override("font_color", Color("8f5f00"))
+		rank_label.add_theme_color_override("font_color", Color("8f5f00"))
+		value_label.add_theme_color_override("font_color", Color("8f5f00"))
 	else:
 		# Opcional: Volver al color original si la fila se recicla
-		name_label.remove_theme_color_override("font_color")
-		rank_label.remove_theme_color_override("font_color")
-		value_label.remove_theme_color_override("font_color")
+		name_label.add_theme_color_override("font_color", Color("4d3402"))
+		rank_label.add_theme_color_override("font_color", Color("4d3402"))
+		value_label.add_theme_color_override("font_color", Color("4d3402"))
