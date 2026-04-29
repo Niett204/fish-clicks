4b783a3ef344b545ba794f7b6b6108901f450c56
Juanfra <juanfra.riera2003@gmail.com>
2026-04-29 17:06:15 +0200

    Panel ranking funcional

--- client/fish-clicks-main/scenes/UI/ranking_row.gd
+++ client/fish-clicks-main/scenes/UI/ranking_row.gd
@@ -6,7 +6,17 @@ extends HBoxContainer
 @onready var value_label = $ValueLabel
 
 func set_data(pos: int, nickname: String, score: String, _foto_base64: String):
-	# Seteamos los textos
 	rank_label.text = str(pos) + "."
 	name_label.text = nickname
 	value_label.text = score
+	
+	if nickname == GlobalData.user_nickname:
+		# Esto cambia el color de la fuente directamente, ignorando el marrón del inspector
+		name_label.add_theme_color_override("font_color", Color(1, 1, 0))
+		rank_label.add_theme_color_override("font_color", Color(1, 1, 0))
+		value_label.add_theme_color_override("font_color", Color(1, 1, 0))
+	else:
+		# Opcional: Volver al color original si la fila se recicla
+		name_label.remove_theme_color_override("font_color")
+		rank_label.remove_theme_color_override("font_color")
+		value_label.remove_theme_color_override("font_color")
